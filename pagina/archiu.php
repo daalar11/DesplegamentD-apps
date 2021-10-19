4537ae24d756931343b1aa460aa22195bf91f7cd
daalar11 <david.daa.2000@gmail.com>
2021-10-19 14:37:41 +0000

    arregla

--- pagina/archiu.php
+++ pagina/archiu.php
@@ -13,7 +13,7 @@ $result = $conn-> query($sql);
 
 if ($result ->num_rows >0){
 	while($row = $result->fetch_assoc()){
-	echo "codi: ". $row["codi"]. " ".$row["nom"]." ".$row["descripcio"]." " .$row["preu"]."<img src='./imatges/" .$row["codi"]. ".jpg'> <br>";
+	echo "codi: ". $row["codi"]. " ".$row["nom"]." ".$row["descripcio"]." " .$row["preu"]."<img src='../.imatges/" .$row["codi"]. ".jpg'> <br>";
 	}
 }else{
 echo "0 results";
