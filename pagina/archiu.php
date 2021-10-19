4f90f66557622f47dfe623867de0eb84cb178abc
David <david.daa.2000@gmail.com>
2021-10-19 18:47:34 +0200

    no se que faig

--- pagina/archiu.php
+++ pagina/archiu.php
@@ -4,7 +4,7 @@ $conn =new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
 	die("Connection failed: ". $conn->connect_error);
 }
-
+echo "<a href='carreto.php'>Torna a inici </a>";
 $sql ="Select codi,nom,descripcio, preu FROM productes";
 $result = $conn-> query($sql);
 
