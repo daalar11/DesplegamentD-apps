b3a06491e15401fd712077ddd1e92c759d245eeb
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-10-27 13:25:23 +0200

    coses

--- pagina/header.php
+++ pagina/header.php
@@ -1,3 +1,3 @@
 <?php
-echo "<header><a href='carreto.php'>Torna a inici </a><button>Castella</button><button>Catala</button> </header>"
+echo "<header><a href='carreto.php'>Torna a inici </a><button>Castella</button><button>Catala</button> </header><hr>"
 ?>
