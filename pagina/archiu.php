ee5a5b4788363ed04de58640b4d0b1f384f4eb89
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-10-27 13:32:12 +0200

    coses

--- pagina/archiu.php
+++ pagina/archiu.php
@@ -15,7 +15,7 @@ $result = $conn-> query($sql);
 <?php
 if ($result ->num_rows >0){
 	while($row = $result->fetch_assoc()){
-	echo "<tr> <td>codi: ". $row["codi"]. "</td><td> ".$row["nom"]."</td><td> ".$row["descripcio"]." </td><td>" .$row["preu"]."<a href='fitxa.php?codi=".$row["codi"]."'><img width='300px' height'300px' src='../.imatges/" .$row["codi"]. ".jpg'></a> <tr>";
+	echo "<tr> <td>codi: ". $row["codi"]. "</td><td> ".$row["nom"]."</td><td> ".$row["descripcio"]." </td><td>" .$row["preu"]." </td><td><a href='fitxa.php?codi=".$row["codi"]."'><img width='300px' height'300px' src='../.imatges/" .$row["codi"]. ".jpg'></a></td> <tr>";
 	}
 }else{
 echo "0 results";
