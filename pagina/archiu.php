60472f4948323512eb7fe483f9a2ffafccf6e195
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-10-27 13:30:37 +0200

    coses

--- pagina/archiu.php
+++ pagina/archiu.php
@@ -8,10 +8,14 @@ include('header.php');
 $sql ="Select codi,nom,descripcio, preu FROM productes";
 $result = $conn-> query($sql);
 
+?>
+<table>
 
+</table>
+<?php
 if ($result ->num_rows >0){
 	while($row = $result->fetch_assoc()){
-	echo "codi: ". $row["codi"]. " ".$row["nom"]." ".$row["descripcio"]." " .$row["preu"]."<a href='fitxa.php?codi=".$row["codi"]."'><img width='300px' height'300px' src='../.imatges/" .$row["codi"]. ".jpg'></a> <br>";
+	echo "<tr> <td>codi: ". $row["codi"]. "</td><td> ".$row["nom"]."</td><td> ".$row["descripcio"]." </td><td>" .$row["preu"]."<a href='fitxa.php?codi=".$row["codi"]."'><img width='300px' height'300px' src='../.imatges/" .$row["codi"]. ".jpg'></a> <tr>";
 	}
 }else{
 echo "0 results";
