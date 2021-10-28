5d7a792ed0324bedf532fc0fdca9154006342376
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-10-28 09:45:34 +0200

    coses

--- pagina/fitxa.php
+++ pagina/fitxa.php
@@ -7,29 +7,27 @@
 	<title>Document</title>
 </head>
 <body>
-
-<a href="archiu.php">Torna a inici </a>>>>
-
 <?php
-$codi=$_GET['codi'];
-include('config-db.php');
-$conn =new mysqli($servername, $username, $password, $dbname);
+	include('header.php');
+	$codi=$_GET['codi'];
+	include('config-db.php');
+	$conn =new mysqli($servername, $username, $password, $dbname);
 
-if($conn->connect_error){
-	die("Connection failed: ". $conn->connect_error);
-}
-	
-$sql ="Select codi,nom,descripcio, preu FROM productes where codi=$codi";
-$result = $conn-> query($sql);
+	if($conn->connect_error){
+		die("Connection failed: ". $conn->connect_error);
+	}
+		
+	$sql ="Select codi,nom,descripcio, preu FROM productes where codi=$codi";
+	$result = $conn-> query($sql);
 
-if ($result ->num_rows >0){
-$row = $result->fetch_assoc();
-    
-	echo "<img width='300px'height='300px' src='../.imatges/" .$row["codi"]. ".jpg'> codi: ". $row["codi"]. " ".$row["nom"]." ".$row["descripcio"]." " .$row["preu"]." <br>";	
-}else{
-	echo "0 results";
-}
-$conn->close();
+	if ($result ->num_rows >0){
+	$row = $result->fetch_assoc();
+		
+		echo "<img width='300px'height='300px' src='../.imatges/" .$row["codi"]. ".jpg'> codi: ". $row["codi"]. " ".$row["nom"]." ".$row["descripcio"]." " .$row["preu"]." <br>";	
+	}else{
+		echo "0 results";
+	}
+	$conn->close();
 ?>
 
 </body>
