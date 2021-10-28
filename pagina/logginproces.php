0ca54896cb17db1fad6a99c6febd76646f9b6ea5
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-10-28 09:18:25 +0200

    coses

--- pagina/logginproces.php
+++ pagina/logginproces.php
@@ -3,11 +3,11 @@ $usuari='adminsitrador';
 $contrasena='1234';
 $usuariUs=$_POST['usuari'];
 $contrasenaUs=$_POST['contrasena'];
-
+echo "$usuariUs . $contrasenaUs";
 
 if(strcmp($usuari,$usuariUs)===0&&strcmp($contrasenaUs,$contrasena)===0){
-    header ("refresh:2;url=form.html");
+    //header ("refresh:2;url=form.html");
 }else{
-    header ("refresh:2;url=loggin.php");
+    //header ("refresh:2;url=loggin.php");
 }
 ?>
