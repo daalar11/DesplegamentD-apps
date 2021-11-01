40ef4aebff74a2b1bba9277c0cb36611d86e5aa9
David <david.daa.2000@gmail.com>
2021-11-01 11:36:53 +0100

    no se que faig

--- pagina/fitxa.php
+++ pagina/fitxa.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 </head>
 <body>
