5e2a87833f04f57c34fed930905abb7e73e22ea2
David <david.daa.2000@gmail.com>
2021-11-02 19:06:54 +0100

    no se que faig

--- pagina/fitxa.php
+++ pagina/fitxa.php
@@ -62,8 +62,8 @@
 echo $selcion_idioma['selecciona'];?>  
 <select onChange="document.location = '<?php echo $_SERVER['PHP_SELF'];?>?idioma=' + this.value" > 
 <option><?php echo $selcion_idioma['idiomas'];?>  
-<option value="es" ></option> <?php echo $selcion_idioma['cast'];?>  
-<option value="cat" ></option> <?php echo $selcion_idioma['cat'];?> 
+<option value="es" > <?php echo $selcion_idioma['cast'];?></option>  
+<option value="cat" > <?php echo $selcion_idioma['cat'];?> </option>
 </select><?php echo actual;?> 
 
 </body>
