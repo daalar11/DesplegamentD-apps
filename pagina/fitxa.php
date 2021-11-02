0e82d358ba1b5885b36684ba575085c936889287
David <david.daa.2000@gmail.com>
2021-11-02 18:54:11 +0100

    no se que faig

--- pagina/fitxa.php
+++ pagina/fitxa.php
@@ -61,7 +61,7 @@
 ?>
  <?php
 echo $selcion_idioma['selecciona'];?>  
-<select onChange="document.location = '<?php echo $_SERVER['PHP_SELF'];?>?idioma=' + this.value" /> 
+<select onChange="document.location = '<?php echo $_SERVER['PHP_SELF'];?>?idioma=' + this.value" > 
 <option><?php echo $selcion_idioma['txt_idiomas'];?>  
 <option value="es" ></option> <?php echo $selcion_idioma['nombre'];?>  
 <option value="en" ></option> <?php echo $selcion_idioma['codigo'];?> 
