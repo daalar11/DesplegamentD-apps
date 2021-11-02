7236242a47f5498c87d1ec57c9b9b982a5b3b8e4
David <david.daa.2000@gmail.com>
2021-11-02 18:53:23 +0100

    no se que faig

--- pagina/fitxa.php
+++ pagina/fitxa.php
@@ -52,7 +52,7 @@
 				<span lang='ca'>preu </span>
 				" .$row["preu"]."<br> 
 				<a href='carreto.php?preu=".$row["preu"]."&codi=".$row["codi"]."'><img width='300px'height='300px' src='../.imatges/" .$row["codi"]. ".jpg'></a>
-			</p>"
+			</p>";
 
 	}else{
 		echo "0 results";
