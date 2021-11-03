476428bacbee653f5ba40a0537f787cc271a678c
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-11-03 12:29:45 +0100

    pagina per formatar

--- pagina/canviaridioma.php
+++ pagina/canviaridioma.php
@@ -1,9 +1,9 @@
 <?php 
 $codi=$_GET['codi'];
 
-if($_COOKIE['idioma']=="es"){
+
     $_COOKIE['idioma']="ca";
     setcookie("idioma","ca");
     header("refresh:1;url=fitxa.php?codi=".$codi);
-}
+
 ?>
